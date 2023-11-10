20b267174237de759a16089f102a71431eacf56e
dailaaaaan <dailanandreufiallos@gmail.com>
2023-11-10 11:48:28 +0100

    he creado el programa con c#

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int NumeroUsuario1 = 0;
+        int NumeroUsuario2 = 0;
+        int total = 0;
+        Console.WriteLine("Dame el primer numero: ");
+        NumeroUsuario1=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Dame el segundo numero: ");
+        NumeroUsuario2=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(total = NumeroUsuario1 + NumeroUsuario2);
+    }
+}
